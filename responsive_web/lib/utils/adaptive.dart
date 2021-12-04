31d0958e5f3cc81aeab6ce26b43cc8795c5cc366
Rammehar <Rammehar@users.noreply.github.com>
2021-12-04 06:12:59 +0530

    responsive

---
+++ responsive_web/lib/utils/adaptive.dart
@@ -0,0 +1,7 @@
+import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
+import 'package:flutter/material.dart';
+
+bool isDisplayDesktop(BuildContext context) =>
+    getWindowType(context) >= AdaptiveWindowType.medium;
+bool isDisplaySmallDesktop(BuildContext context) =>
+    getWindowType(context) == AdaptiveWindowType.medium;
